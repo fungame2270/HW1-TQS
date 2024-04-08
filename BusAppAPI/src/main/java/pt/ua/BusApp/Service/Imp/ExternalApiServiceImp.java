3c29a74d2e6630bdb1e7690683764e839b8d8bea
Tomas Victal <tomasvictal@gmail.com>
2024-04-08 12:16:39 +0100

    fix url

--- BusAppAPI/src/main/java/pt/ua/BusApp/Service/Imp/ExternalApiServiceImp.java
+++ BusAppAPI/src/main/java/pt/ua/BusApp/Service/Imp/ExternalApiServiceImp.java
@@ -3,6 +3,11 @@ package pt.ua.BusApp.Service.Imp;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 import org.json.JSONObject;
 
 import pt.ua.BusApp.Service.ExternalApiService;
@@ -19,10 +24,14 @@ public class ExternalApiServiceImp implements ExternalApiService {
     @Cacheable("currency")
     @Override
     public Double getConvertRation(String currency){
-        String uri = String.format("https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_vySWiU7PfKpdVylXUdnf4aVaKSdFEA7QdVNHpyUX&currencies=%s",currency);
-        String restponse =  restTemplate.getForObject(uri, String.class);
-        JSONObject jsonObject = new JSONObject(restponse);
-        return jsonObject.getJSONObject("data").getDouble(currency);
+        List<String> currencys = Arrays.asList("USD","EUR","CAD","JPY");
+        if (currencys.contains(currency)){
+            String uri = String.format("https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_vySWiU7PfKpdVylXUdnf4aVaKSdFEA7QdVNHpyUX&currencies=%s",currency);
+            String restponse =  restTemplate.getForObject(uri, String.class);
+            JSONObject jsonObject = new JSONObject(restponse);
+            return jsonObject.getJSONObject("data").getDouble(currency);
+        }
+        return -1.0;
     }
     
 }
